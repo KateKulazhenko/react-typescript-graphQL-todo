58b21023b32fcaf697a96e61b2cbeafda38a6317
Katsiaryna Lunchuk <katsiarynalunchuk@mac-205s-Mac-mini.local>
2018-10-11 17:14:11 +0300

    Fix webpack

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    console.log(123);
+    return <h1>Hello!</h1>;
+  }
+}
+
+export default Hello;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,42 @@
-let path = require("path");
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  context: path.join(__dirname, 'src'),
+  resolve: {
+    modules: [path.resolve('./src'), 'node_modules'],
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  entry: {
+    app: './index.tsx',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
   module: {
-    entry: {
-      app: "./index.tsx"
-    },
     rules: [
       {
         test: /\.tsx$/,
         exclude: /node_modules/,
-        loader: "awesome-typescript-loader"
-      }
-    ]
-  }
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  // For development https://webpack.js.org/configuration/devtool/#for-development
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    port: 3000,
+    noInfo: true,
+    stats: 'errors-only',
+  },
+  plugins: [
+    // Generate index.html in /dist =>
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+      hash: true,
+    }),
+  ],
 };
